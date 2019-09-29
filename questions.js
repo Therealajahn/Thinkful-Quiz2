46cefa65ca131b3a735420d10987b581882644cd
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-09-29 13:49:05 -0400

    rendered questions and incremented through them

--- questions.js
+++ questions.js
@@ -1,1 +1,80 @@
-document.write("no");
+const QUESTIONS = [
+    {index:1},
+    {
+        question: "Who is the creator of the 2010 record Cosmogramma?",
+        answers: ["Cosmo Gramma",
+                  "Alphonso “Cosmo” Gramton",
+                  "Steven Ellison",
+                  "The artist known as “D” "],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "Who’s mother died before Cosmogramma was produced?",
+        answers: ["Billions, too many to list here, but specifically, Steven Ellison’s",
+                  "Alphonso “Cosmo” Gramton’s",
+                  "Thundercat’s",
+                  "Thom Yorke’s "],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "What kind of music is included on this album?",
+        answers: ["Hawaiian Metal",
+                  "Psychedelic Hip-Hop",
+                  "Kitchen Sink",
+                  "Utopian Virtual"],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "Which artist provided some bass lines and vocals for the album?",
+        answers: ["Thundercat",
+                  "Squarechubs",
+                  "Davie504",
+                  "Flea"],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "Which of the following is the name of a track that does not appear on the album?",
+        answers:                                 ["Satelllliiiiiiiteee",
+                  "Dance of the Pseudo Nymph",
+                  "Mmmhmm feat. Thundercat",
+                  "Yes"],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "Which of the following artists do not contribute to the album?",
+        answers:                                 ["Dorian Concept",
+                  "Alphonso “Cosmo” Gramton",
+                  "Low Leaf",
+                  "Rebekah Raff"],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "How was the album promoted?",
+        answers:                                 [
+        "It was bundled with an augmented reality app called Cosmogramma Fieldlines, released for free",
+        "Steven Ellison convinced Neil deGrasse Tyson to wear a Cosmogramma tie on the Joe Rogan podcast, when Rogan asked him about it, he said that the album simulated the feeling of being inside of a star that collapses into a black hole.",
+                  "Facebook ads",
+                  "Cheap plastic “vinyl” albums were produced, and the Warp album label members banded together to throw the albums frisbee-style all over LA "],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+    {
+        question: "Which is not an example of Ellison’s vision for the album?",
+        answers:                                 ["He wanted to make music that was simple with a “less-is-more” approach",
+                  "He wanted to make a map of the universe",
+                  "He was inspired by lucid dreaming",
+                  "He wanted to fuse 20th and 21st century music"],
+        buttons: ["Submit",
+                  "Reset Quiz"]
+    },
+
+];
+console.log(QUESTIONS[0].question);
+
+    
--- quiz2.js
+++ quiz2.js
@@ -1,12 +1,74 @@
-function rotateTriangle() {
-    
-    let containerArray = document.getElementsByClassName('js-container');
-    
-    let angle = 10;
-    
-    containerArray.forEach((item)=>{
-        item.style.cssText = `transform: rotate(10deg);`;
-    });
+
+
+function makeContainers() {
+    for(let i = 0; i < 10; i++) {
+        let container = document.createElement('div');
+       container.setAttribute("class", `container js-container-${i}`); 
+        
+       makeTriangles(i); document.body.appendChild(triangle);
+       rotateTriangles(i, triangle);    
+    }
 }
 
-rotateTriangle();
+function makeTriangles(index) {
+    let triangle = document.createElement('div');
+    triangle.setAttribute("class",`triangle js-triangle-${i}`)
+}
+
+function rotateTriangles(index, triangle) {
+   let angle = index * Math.PI;
+   triangle.style.transform =               "rotate(${angle}deg)";   
+}
+
+
+function incrementQuestions(){
+    console.log(QUESTIONS[0].index);
+    $(".Submit").on("click",()=>{
+        event.preventDefault();
+        QUESTIONS[0].index++;
+        console.log(QUESTIONS[0].index)
+
+        renderQuestions();
+    });
+    
+    
+}
+
+
+function renderQuestions() {
+    
+    let question = `<header class='js-question question'>${QUESTIONS[QUESTIONS[0].index].question}</header>`
+    
+    $("form").html(question);
+    
+    renderAnswers();
+}
+
+function renderAnswers() {
+    
+    let answers = QUESTIONS[QUESTIONS[0].index].answers
+    
+    answers.forEach((item)=>{
+        $("form").append(`<input type='radio' name="answers" class="answers" value='A'>${item}<br>`)
+    })
+    renderButtons();
+}
+
+function renderButtons() {
+    
+    let buttons = 
+    QUESTIONS[QUESTIONS[0].index].buttons;
+    
+    $("form").append(`<div class="button-wrap></div>`)
+    
+    buttons.forEach((item)=>{
+        $(".button-wrap").append(`
+    
+    <button class="${item}">${item}</button>`)
+    })
+    incrementQuestions();
+}
+
+$(()=>{
+    renderQuestions();
+})
