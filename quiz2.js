deffdd750561cb91c163a46eb781a59d663fbd90
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-12-06 15:30:46 -0500

    rendered answers with a map from the STORE

--- quiz2.js
+++ quiz2.js
@@ -36,100 +36,82 @@ function handleSubmit(){
         
     });
     
-    
+   renderQuestions(); 
 }
 
 
-//function renderQuestions() {
-//    
-//    let question = `<header class='js-question question'>${STORE.questions[STORE.index].question}</header>`
-//    
-//    $(".flex").html(question);
-//    
-//    renderAnswers();
-//}
-//
-//function renderAnswers() {
-//    
-//    let answers = STORE.questions[STORE.index].answers
-//    
-//    $(".flex").append('<div class="answers"></div>')
-//    
-//    answers.forEach((item,index)=>{
-//        $(".answers").append(`
-//<input type='radio' name="answers" value='A' id="option${index}" ><label for="option${index}">${item}</label><br>`)
-//    })
-//            
-//    renderStats();
-//};
-//                     
-//function renderStats(){
-//    
-//    makeStats();
-//    
-//    let correct = stats.correct;
-//    let incorrect = stats.incorrect;
-//    let questions = stats.questions;
-//    let questionsDone = stats.questionsDone;
-//    
-//    $("form").append("<div class='js-stats'></div>")
-//    
-//    $(".js-stats").html(`<p class="correct">${correct} out of ${questions} correct</p>
-//            <p class="incorrect">${incorrect} out of ${questions} incorrect</p>
-//            <p class="questions">${questionsDone} out of ${questions} questions</p>`);
-//    
-//     renderButtons();
-//}
-//function renderButtons() {
-//    
-//    let buttons = 
-//    STORE.questions[STORE.index].buttons;
-//    
-//    
-//    buttons.forEach((item)=>{
-//        $("form").append(`
-//    
-//    <button class="${item}">${item}</button>`)
-//    })
-//    handleSubmit();
-//}
+
+function renderAnswers() {
+    
+    let answers = STORE.questions[STORE.index].answers
+    
+    $(".flex").append('<div class="answers"></div>')
+    
+    answers.forEach((item,index)=>{
+        $(".answers").append(`
+<input type='radio' name="answers" value='A' id="option${index}" ><label for="option${index}">${item}</label><br>`)
+    })
+            
+    renderStats();
+};
+                     
+function renderStats(){
+    
+    makeStats();
+    
+    let correct = stats.correct;
+    let incorrect = stats.incorrect;
+    let questions = stats.questions;
+    let questionsDone = stats.questionsDone;
+    
+    $("form").append("<div class='js-stats'></div>")
+    
+    $(".js-stats").html(`<p class="correct">${correct} out of ${questions} correct</p>
+            <p class="incorrect">${incorrect} out of ${questions} incorrect</p>
+            <p class="questions">${questionsDone} out of ${questions} questions</p>`);
+    
+     renderButtons();
+}
+function renderButtons() {
+    
+    let buttons = 
+    STORE.questions[STORE.index].buttons;
+    
+    
+    buttons.forEach((item)=>{
+        $("form").append(`
+    
+    <button class="${item}">${item}</button>`)
+    })
+    handleSubmit();
+}
 
 function renderQuestions() {
-    console.log("renderQuestions");
+    console.log("renderQuestionsStatic");
+    let options = STORE.questions[STORE.index].answers.map((question, i) => (
+    `<div>
+            <input type="radio" name="answers" value="A" id="option${i}">
+            <label for="option${i}">${question}</label><br>
+    </div>`
+    ))
     $("body").html(
         
             `<form class="form">
         
-            <header class="js-question  question">Who is the creator of the 2010 record Cosmogramma?</header>
+            <header class="js-question  question">${STORE.questions[STORE.index].question}</header>
                
             <div class="answers">
 
-            <div>
-            <input type="radio" name="answers" value="A" id="option0">
-            <label for="option0">Cosmo Gramma</label><br>
-            </div>
-            
-            <div>    
-            <input type="radio" name="answers" value="A" id="option1"><label for="option1">Alphonso “Cosmo” Gramton</label><br>
-            </div>
-            
-            <div>
-            <input type="radio" name="answers" value="A" id="option2"><label for="option2">Steven Ellison</label><br>
-            </div>
-            
-            <div>    
-            <input type="radio" name="answers" value="A" id="option3"><label for="option3">The artist known as “D” </label><br>
-            </div>    
-                
+            ${options.join('')}
             
         </div>
 
            
         <div class="js-stats stats">
           <div>
-            <p class="correct">0 out of 10 correct</p>
-            <p class="incorrect">0 out of 10 incorrect</p>
-            <p class="questions">1 out of 10 questions</p>
+            <p class="correct">${stats.correct} out of 10 correct</p>
+            <p class="incorrect">${stats.incorrect} out of 10 incorrect</p>
+            <p class="questions">${stats.questions} out of 10 questions</p>
           </div>
         </div>
     
